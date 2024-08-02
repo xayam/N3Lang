55240e730450e0126ea2f0c80bc55aea91ca3063
xayam <xayam@yandex.ru>
2024-08-02 13:29:43 +0300

    v0.20

--- example/example_001_swaps.py
+++ example/example_001_swaps.py
@@ -32,14 +32,15 @@ def main(_data: str, verbose=1) -> [list, list]:
 
 if __name__ == "__main__":
     # for data in ["0110", "1001"]: # ["101011"]:# ["011000100111", "111111000000"]:
-    for width in range(1, 5):
+    for width in range(11, 12):
         for data in range(2 ** width):
             arr = f"{data:{width}b}".replace(" ", "0")
-            i, o = main(arr, verbose=0)
+            i, o = main(arr, verbose=1)
             res = o == i
-            if not res:
-                i, o = main(arr, verbose=1)
-                print("Result: " + colorize_bool(i == o))
-                print(f"Input data for compress: {i}")
-                print(f"Output decompress data: {o}")
-                print("")
+            # if not res:
+            #     i, o = main(arr, verbose=1)
+            print("Result: " + colorize_bool(res))
+            print(f"Input data for compress: {i}")
+            print(f"Output decompress data: {o}")
+            print("")
+            input("[ENTER]:")
