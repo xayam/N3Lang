62798eba55a107a3fc90e7bf95539bb711e0bace
xayam <xayam@yandex.ru>
2024-08-05 09:28:18 +0300

    v0.102

--- n3lang/n3validation.py
+++ n3lang/n3validation.py
@@ -71,9 +71,8 @@ def main(degrees=None, verbose=0) -> str:
 
 if __name__ == "__main__":
     # ? P(W)
-    # P(W) = LIM SUM W/LOG2[W + 1], W = 0 to LOG2[2^N - 1] as N->+Infinity
-    # W <= P(W)
-    # 0.5 <= (Limit[Sum[Log[2, W + 1], {W, 0, Log[2, 2^N - 1]}]] as N->+Infinity) > 0
+    # P(W) = Limit[Sum[Log[2, W + 1], {W, 0, Log[2, 2 ^ N - 1]}]] as N->+Infinity
+    # 0.5 <= P(W) > 0
 
     # main(degrees=[3, 9, 23, 55], verbose=1)
     # main(verbose=1)
